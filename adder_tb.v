e816d067a8eb6dd8d640f2621b5de4d27c58a162
TonyKorol1 <91504364+TonyKorol1@users.noreply.github.com>
2023-04-26 16:25:39 -0700

    rip

--- adder_tb.v
+++ adder_tb.v
@@ -19,7 +19,7 @@ fourBitAdder_TwoByTwo f0(
 
 initial begin
 // Students: test a set of inputs for the `fullAdder` module
-    for (i = 0; i < 32; i = i + 1) begin
+    for (i = 0; i < 512; i = i + 1) begin
         A[0] = i % 2;
         B[0] = (i % 4) / 2;
         A[1] = (i % 8) / 4;
