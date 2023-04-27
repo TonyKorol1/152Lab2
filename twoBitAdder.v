b9419b9eb8f19477ee385bab2c90eabaed850770
Peak1260 <92136161+Peak1260@users.noreply.github.com>
2023-04-27 13:45:26 -0700

    Add files via upload

--- twoBitAdder.v
+++ twoBitAdder.v
@@ -8,7 +8,7 @@ module twoBitAdder (
 
     // Students: Implementation goes here
     
-    assign cout = (cin & a[1] & b[0]) | (cin & a[1] & b[1]) | (cin & a[0] & b[1]) | (cin & a[0] & a[1]) | (cin & b[0] & b[1])|(a[0] & a[1] & b[0]) | (a[0] & b[0] & b[1]) | (a[0] & a[1] & b[1]) | (a[1] & b[0] & b[1]) ;
+    assign cout = (cin & a[1] & a[0]) | (cin & b[1] & b[0]) | (cin & a[1] & b[0]) | (cin & a[0] & b[1]) | (a[0] & a[1] & b[0]) | (a[0] & b[0] & b[1]) | (a[1] & b[1]);
     
     assign s[0] = (~cin & a[0] & ~b[0]) | (~cin & ~a[0] & b[0]) | (cin & ~a[0] & ~b[0]) | (cin & a[0] & b[0]);
     
